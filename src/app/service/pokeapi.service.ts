25eb64fbfb8a5683de7ccf09a29e10028dd636d7
JaxV9 <layanj.pro@gmail.com>
2024-09-08 11:57:10 +0200

    feat: pokeapi service added

---
+++ src/app/service/pokeapi.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokeapiService {
+
+  url = 'https://pokeapi.co/api/v2/pokemon/';
+  constructor(private http: HttpClient) {}
+  getJoke(): Observable<any> {
+    return this.http.get(this.url, { headers: { Accept: 'application/json' } });
+  }
+}
